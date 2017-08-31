dfc208d58245a89babf766dbf7bb2c6b4cd543a3
BeedleEiEi <58070023@kmitl.ac.th>
2017-08-31 22:42:16 +0700

    backup

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/chanoknan58070023/simplemydot/MainActivity.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/chanoknan58070023/simplemydot/MainActivity.java
@@ -1,7 +1,12 @@
 package kmitl.lab03.chanoknan58070023.simplemydot;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -14,12 +19,16 @@ import kmitl.lab03.chanoknan58070023.simplemydot.view.DotView;
 
 public class MainActivity extends AppCompatActivity implements Dot.OnDotChangedListener {
 
+    final Context context = this;
     private DotView dotView;
     private Dot dot;
     private ArrayList<Dot> listDot;
     private ArrayList<Dot> listDotTouch;
     private int color;
     private float eXis, eYis;
+    private float pxis, pyis;
+    private int rad = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +39,47 @@ public class MainActivity extends AppCompatActivity implements Dot.OnDotChangedL
         dotView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                Random rand = new Random();
+                rad = rand.nextInt(70) + 80;
+                if (isOverlap(event.getX(), event.getY(), rad)) {
+                    return true;
+                }
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    eXis = event.getX();
+                    eYis = event.getY();
+                    addDot(rad);
+                }
                 System.out.println("Touched");
-                eXis = event.getX();
-                eYis = event.getY();
-                addDot();
-//                System.out.println(listDot + "  , at MainAct Touched"); //add in
+
                 return true;
             }
         });
     }
-    public void onRandomDot(View view) {
 
-        dot = new Dot(this, 0, 0, 30);
+
+    public void alertDialog(final Dot dot) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+        alertDialog.setTitle("Select Action");
+        alertDialog.setMessage("Choose").setCancelable(true).setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //delete item
+                listDot.remove(dot);
+                dotView.invalidate();
+            }
+        }).setNegativeButton("Edit Color", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+    }
+
+
+
+    public void onRandomDot(View view) {
         Random rand = new Random();
+        dot = new Dot(this, 0, 0, 30);
         dot.setColor();
         int centerX = rand.nextInt(830) + 30;
         int centerY = rand.nextInt(1200) + 30;
@@ -52,9 +89,21 @@ public class MainActivity extends AppCompatActivity implements Dot.OnDotChangedL
 //        System.out.println(listDot + "  , at MainAct"); //add in
     }
 
-    public void addDot(){
-        dot = new Dot(this, 0, 0, 80);
-        Random rand = new Random();
+
+    public boolean isOverlap(float px, float py, float rad) {
+        for (Dot dot : listDot) {
+            double distance = Math.pow(Math.pow(dot.getCenterX() - px, 2) + Math.pow(dot.getCenterY() - py, 2), 0.5);
+            if (distance <= rad) {
+                //alert
+                alertDialog(dot);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addDot(int radius) {
+        dot = new Dot(this, 0, 0, radius);
         dot.setColor();
         float centerX = eXis;
         float centerY = eYis;
--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/chanoknan58070023/simplemydot/view/DotView.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/chanoknan58070023/simplemydot/view/DotView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.ContextMenu;
 import android.view.MotionEvent;
 import android.view.View;
 
